576213631323b88fedbe5b4571104053866b6a9c
Tadahiro Ishisaka <tishisaka@baleen.studio>
2025-06-13 12:51:23 +0900

    build: 第6章のためのGoモジュールと依存関係を追加

---
+++ ch06/internal/server/server.go
@@ -0,0 +1,241 @@
+package server
+
+import (
+	"context"
+	"time"
+
+	api "github.com/ishisaka/go_distribute/proglog/api/v1"
+
+	grpcMiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpcAuth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpcZap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpcCtxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+
+	"go.opencensus.io/plugin/ocgrpc"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
+)
+
+// Config は gRPC サーバー構築時に必要な設定情報を保持する構造体です。
+// CommitLog と Authorizer を管理します。
+type Config struct {
+	CommitLog  CommitLog
+	Authorizer Authorizer
+}
+
+const (
+	objectWildcard = "*"
+	produceAction  = "produce"
+	consumeAction  = "consume"
+)
+
+// Authorizer インターフェースは、特定の主題、対象、アクションに対するアクセスを許可または拒否する機能を提供します。
+// 主に認可ロジックの実装を目的としています。
+type Authorizer interface {
+	Authorize(subject, object, action string) error
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+// grpcServer は gRPC サーバーの主要な構造体です。
+// api.UnimplementedLogServer を埋め込み、LogServer インターフェースに準拠します。
+// Config を利用してログ操作を管理します。
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+// CommitLog は、ログへのデータの追加と読み取りを管理するインターフェースです。
+// Append メソッドでデータを追加し、Read メソッドで特定のオフセットのデータを読み取ります。
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint64) (*api.Record, error)
+}
+
+// NewGRPCServer は、新しい gRPC サーバーを作成して返す関数です。
+// 指定された設定および任意の gRPC サーバーオプションを使用して初期化されます。
+// Config 構造体に基づいて grpcServer を生成し、LogServer として登録します。
+// エラーが発生した場合は nil とエラーを返します。
+func NewGRPCServer(config *Config, grpcOpts ...grpc.ServerOption) (
+	*grpc.Server,
+	error,
+) {
+	// Zapの設定
+	logger := zap.L().Named("server")
+	zapOpts := []grpcZap.Option{
+		grpcZap.WithDurationField(
+			func(duration time.Duration) zapcore.Field {
+				return zap.Int64(
+					"grpc.time_ns",
+					duration.Nanoseconds(),
+				)
+			},
+		),
+	}
+
+	// OpenCensusがメトリクスとトレースを設定
+	// 全てのリクエストをトレース
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+	err := view.Register(ocgrpc.DefaultServerViews...)
+	if err != nil {
+		return nil, err
+	}
+
+	grpcOpts = append(grpcOpts, grpc.StreamInterceptor(
+		grpcMiddleware.ChainStreamServer(
+			// インターセプターとしてZapログを組み込む
+			grpcCtxtags.StreamServerInterceptor(),
+			grpcZap.StreamServerInterceptor(logger, zapOpts...),
+			// インターセプターとしてauthenticateを組み込む
+			grpcAuth.StreamServerInterceptor(authenticate),
+		)), grpc.UnaryInterceptor(grpcMiddleware.ChainUnaryServer(
+		grpcCtxtags.UnaryServerInterceptor(),
+		grpcZap.UnaryServerInterceptor(logger, zapOpts...),
+		grpcAuth.UnaryServerInterceptor(authenticate),
+	)),
+		grpc.StatsHandler(&ocgrpc.ServerHandler{}),
+	)
+	gsrv := grpc.NewServer(grpcOpts...)
+	srv, err := newgrpcServer(config)
+	if err != nil {
+		return nil, err
+	}
+	api.RegisterLogServer(gsrv, srv)
+	return gsrv, nil
+}
+
+// newgrpcServer は、新しい gRPC サーバーを作成し、初期化します。
+// Config 構造体を受け取り、その設定を使用して grpcServer を生成します。
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+// Produce メソッドは、指定されたリクエストに基づき新しいレコードをログに追加し、結果のオフセットをレスポンスとして返します。
+// コンテキストを受け取り、エラーが発生した場合は nil とエラーを返します。
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
+	*api.ProduceResponse, error) {
+	// 認可できるのかの確認
+	if err := s.Authorizer.Authorize(
+		subject(ctx),
+		objectWildcard,
+		produceAction,
+	); err != nil {
+		return nil, err
+	}
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+// Consume メソッドは指定されたオフセットからログレコードを読み取り、レスポンスとして返します。
+// エラーが発生した場合は nil とエラーを返します。
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
+	*api.ConsumeResponse, error) {
+	// 認可できるかの確認
+	if err := s.Authorizer.Authorize(
+		subject(ctx),
+		objectWildcard,
+		consumeAction,
+	); err != nil {
+		return nil, err
+	}
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+// ProduceStream は双方向ストリーミングを実現する RPC メソッドです。リクエストを受信しレスポンスを送信します。
+// ストリーム内でエラーが発生した場合、その時点で処理を終了しエラーを返却します。
+// 各リクエストは Produce メソッドを呼び出すことで処理されます。
+func (s *grpcServer) ProduceStream(
+	stream api.Log_ProduceStreamServer,
+) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
+
+// ConsumeStream はサーバーストリーミング RPC を処理し、指定されたオフセットのログレコードを継続的に送信します。
+// クライアントがストリームを終了させると、処理を終了して nil を返します。
+// 無効なオフセットの場合、適切なエラーハンドリングを行い、処理を続行します。
+func (s *grpcServer) ConsumeStream(
+	req *api.ConsumeRequest,
+	stream api.Log_ConsumeStreamServer,
+) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case api.ErrOffsetOutOfRange:
+				continue
+			default:
+				return err
+			}
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
+}
+
+// authenticate は gRPC の認証用インターセプタ関数です。
+// コンテキストからクライアント情報を取得し、認証情報に基づいて主題を設定します。
+// 必要な認証情報が不足している場合でも、エラーではなく適切な値を設定して処理を継続します。
+// 主題情報はコンテキストに保存され、後続の処理で利用されます。
+// エラーが発生した場合は context.Context と共にエラーを返却します。
+func authenticate(ctx context.Context) (context.Context, error) {
+	p, ok := peer.FromContext(ctx)
+	if !ok {
+		return ctx, status.New(
+			codes.Unknown,
+			"couldn't find peer info",
+		).Err()
+	}
+
+	if p.AuthInfo == nil {
+		return context.WithValue(ctx, subjectContextKey{}, ""), nil
+	}
+
+	tlsInfo := p.AuthInfo.(credentials.TLSInfo)
+	subject := tlsInfo.State.VerifiedChains[0][0].Subject.CommonName
+	ctx = context.WithValue(ctx, subjectContextKey{}, subject)
+
+	return ctx, nil
+}
+
+// subject はコンテキストから現在の認証主体 (subject) を取得します。
+func subject(ctx context.Context) string {
+	return ctx.Value(subjectContextKey{}).(string)
+}
+
+type subjectContextKey struct{}
---
+++ ch06/internal/server/servet_test.go
@@ -0,0 +1,336 @@
+package server
+
+import (
+	"flag"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	"context"
+	"github.com/ishisaka/go_distribute/proglog/internal/auth"
+	"go.opencensus.io/examples/exporter"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
+
+	api "github.com/ishisaka/go_distribute/proglog/api/v1"
+	"github.com/ishisaka/go_distribute/proglog/internal/config"
+	"github.com/ishisaka/go_distribute/proglog/internal/log"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+var debug = flag.Bool("debug", false, "Enable observability for debugging.")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if *debug {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+		zap.ReplaceGlobals(logger)
+	}
+	os.Exit(m.Run())
+}
+
+// TestServer はサーバーの動作を異なるシナリオでテストするための関数です。
+// 各シナリオごとに対応するテスト関数が実行されます。
+// テストごとに設定を初期化し、後処理を行います。
+func TestServer(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		rootClient api.LogClient,
+		nobodyClient api.LogClient,
+		config *Config,
+	){
+		"produce/consume a message to/from the log succeeds": testProduceConsume,
+		"produce/consume stream succeeds":                    testProduceConsumeStream,
+		"consume past log boundary fails":                    testConsumePastBoundary,
+		"unauthorized fails":                                 testUnauthorized,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			rootClient,
+				nobodyClient,
+				config1,
+				teardown := setupTest(t, nil)
+			defer teardown()
+			fn(t, rootClient, nobodyClient, config1)
+		})
+	}
+}
+
+// setupTest はテスト用の設定を初期化し、必要なクライアント、設定、後処理関数を提供します。
+// gRPCサーバーやクライアントの作成、TLS設定、リスナーの準備を行います。
+// rootClient, nobodyClient はそれぞれ異なる認証情報を持つクライアントです。
+// cfg には gRPC サーバーの設定情報が格納され、teardown はリソースを解放する関数です。
+// テストケースでの利用が想定され、t.Helper() によりヘルパー関数として登録されます。
+func setupTest(t *testing.T, fn func(*Config)) (
+	rootClient api.LogClient,
+	nobodyClient api.LogClient,
+	cfg *Config,
+	teardown func(),
+) {
+	t.Helper()
+
+	// リスナーの作成
+	l, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	// クライアントの作成
+	newClient := func(crtPath, keyPath string) (
+		*grpc.ClientConn,
+		api.LogClient,
+		[]grpc.DialOption,
+	) {
+		tlsConfig, err := config.SetupTLSConfig(config.TLSConfig{
+			CertFile: crtPath,
+			KeyFile:  keyPath,
+			CAFile:   config.CAFile,
+			Server:   false,
+		})
+		require.NoError(t, err)
+		tlsCreds := credentials.NewTLS(tlsConfig)
+		opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+		conn, err := grpc.NewClient(l.Addr().String(), opts...)
+		require.NoError(t, err)
+		client := api.NewLogClient(conn)
+		return conn, client, opts
+	}
+
+	// rootクライアント作成
+	var rootConn *grpc.ClientConn
+	rootConn, rootClient, _ = newClient(
+		config.RootClientCertFile,
+		config.RootClientKeyFile,
+	)
+
+	// nobodyクライアントの作成
+	var nobodyConn *grpc.ClientConn
+	nobodyConn, nobodyClient, _ = newClient(
+		config.NobodyClientCertFile,
+		config.NobodyClientKeyFile,
+	)
+
+	// gRPCサーバーのTLS（Transport Layer Security）設定
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: l.Addr().String(),
+		Server:        true,
+	})
+	require.NoError(t, err)
+	serverCreds := credentials.NewTLS(serverTLSConfig)
+
+	// log（ロジック）の作成
+	dir, err := os.MkdirTemp("", "server-test")
+	require.NoError(t, err)
+
+	clog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	// 作ったlogをgRPC ServerのConfigに設定
+	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
+	cfg = &Config{
+		CommitLog:  clog,
+		Authorizer: authorizer,
+	}
+	if fn != nil {
+		fn(cfg)
+	}
+
+	var telemetryExporter *exporter.LogExporter
+	if *debug {
+		metricsLogFile, err := os.CreateTemp("", "metrics-*.log")
+		require.NoError(t, err)
+		t.Logf("metrics log file: %s", metricsLogFile.Name())
+
+		tracesLogFile, err := os.CreateTemp("", "traces-*.log")
+		require.NoError(t, err)
+		t.Logf("traces log file: %s", tracesLogFile.Name())
+
+		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
+			MetricsLogFile:    metricsLogFile.Name(),
+			TracesLogFile:     tracesLogFile.Name(),
+			ReportingInterval: time.Second,
+		})
+		require.NoError(t, err)
+		err = telemetryExporter.Start()
+		require.NoError(t, err)
+	}
+
+	// gRPC Serverの作成
+	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
+	require.NoError(t, err)
+
+	// goルーチンでサーバーを動かす
+	go func() {
+		_ = server.Serve(l)
+	}()
+
+	// クライアントとConfig, 後処理のクロージャーを返す
+	return rootClient, nobodyClient, cfg, func() {
+		_ = rootConn.Close()
+		_ = nobodyConn.Close()
+		server.Stop()
+		_ = l.Close()
+		if telemetryExporter != nil {
+			time.Sleep(1500 * time.Millisecond)
+			telemetryExporter.Stop()
+			telemetryExporter.Close()
+		}
+	}
+}
+
+// testProduceConsume はメッセージの生成と消費をテストします。
+// クライアントを使用してログにメッセージを生成し、その後消費することで正しい動作を検証します。
+// テストデータは Produce と Consume の過程で正確に処理されることを確認します。
+func testProduceConsume(t *testing.T, client, _ api.LogClient, _ *Config) {
+	ctx := context.Background()
+
+	want := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	produce, err := client.Produce(
+		ctx,
+		&api.ProduceRequest{
+			Record: want,
+		},
+	)
+	require.NoError(t, err)
+	want.Offset = produce.Offset
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+		Offset: produce.Offset,
+	})
+	require.NoError(t, err)
+	require.Equal(t, want.Value, consume.Record.Value)
+	require.Equal(t, want.Offset, consume.Record.Offset)
+}
+
+// testConsumePastBoundary はログ境界を越えた取得操作が失敗することをテストする関数です。
+func testConsumePastBoundary(
+	t *testing.T,
+	client,
+	_ api.LogClient,
+	_ *Config,
+) {
+	ctx := context.Background()
+
+	produce, err := client.Produce(ctx, &api.ProduceRequest{
+		Record: &api.Record{
+			Value: []byte("hello world"),
+		},
+	})
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+		Offset: produce.Offset + 1,
+	})
+	if consume != nil {
+		t.Fatal("consume not nil")
+	}
+	got := status.Code(err)
+	want := codes.OutOfRange
+	if got != want {
+		t.Fatalf("got err: %v, want: %v", got, want)
+	}
+}
+
+// testProduceConsumeStream は、クライアントでのストリーム型プロデューサーとコンシューマーの動作をテストします。
+// クライアントがメッセージのストリームを正常に送受信するかを確認します。
+// テストでは、複数のメッセージの送信やオフセットの照合を行います。
+func testProduceConsumeStream(
+	t *testing.T,
+	client,
+	_ api.LogClient,
+	_ *Config,
+) {
+	ctx := context.Background()
+
+	records := []*api.Record{{
+		Value:  []byte("first message"),
+		Offset: 0,
+	}, {
+		Value:  []byte("second message"),
+		Offset: 1,
+	}}
+
+	{
+		stream, err := client.ProduceStream(ctx)
+		require.NoError(t, err)
+
+		for offset, record := range records {
+			err = stream.Send(&api.ProduceRequest{
+				Record: record,
+			})
+			require.NoError(t, err)
+			res, err := stream.Recv()
+			require.NoError(t, err)
+			if res.Offset != uint64(offset) {
+				t.Fatalf(
+					"got offset: %d, want: %d",
+					res.Offset,
+					offset,
+				)
+			}
+		}
+
+	}
+
+	{
+		stream, err := client.ConsumeStream(
+			ctx,
+			&api.ConsumeRequest{Offset: 0},
+		)
+		require.NoError(t, err)
+
+		for i, record := range records {
+			res, err := stream.Recv()
+			require.NoError(t, err)
+			require.Equal(t, res.Record, &api.Record{
+				Value:  record.Value,
+				Offset: uint64(i),
+			})
+		}
+	}
+}
+
+// testUnauthorized は、未認証のクライアントがリソース操作を試みた際にエラーが発生することを確認するテストです。
+// クライアントの Produce および Consume メソッドが PermissionDenied エラーを返すことを検証します。
+func testUnauthorized(
+	t *testing.T,
+	_,
+	client api.LogClient,
+	_ *Config,
+) {
+	ctx := context.Background()
+	produce, err := client.Produce(ctx,
+		&api.ProduceRequest{
+			Record: &api.Record{
+				Value: []byte("hello world"),
+			},
+		},
+	)
+	if produce != nil {
+		t.Fatalf("produce response should be nil")
+	}
+	gotCode, wantCode := status.Code(err), codes.PermissionDenied
+	if gotCode != wantCode {
+		t.Fatalf("got code: %d, want: %d", gotCode, wantCode)
+	}
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+		Offset: 0,
+	})
+	if consume != nil {
+		t.Fatalf("consume response should be nil")
+	}
+	gotCode, wantCode = status.Code(err), codes.PermissionDenied
+	if gotCode != wantCode {
+		t.Fatalf("got code: %d, want: %d", gotCode, wantCode)
+	}
+}
